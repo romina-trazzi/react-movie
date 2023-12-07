0c8ee5fc4ba7ca5ba3af5a0b8b467e32c3c8e471
romina-trazzi <trazzi.romina@gmail.com>
2023-12-07 04:05:38 +0100

    Prova

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css'
 function App() {
 
     return (
-
-        <h1>Title</h1>
-    )
+       <div>
+            <h1>Title</h1>
+            <h1>Prova</h1>
+    )   </div>
 }
 
 export default App
