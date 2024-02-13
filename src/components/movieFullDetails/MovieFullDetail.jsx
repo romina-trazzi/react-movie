5d39942b739d4c6483871a7bcaa36247470535ee
Romina Trazzi <romina.trazzi@posta.it>
2024-02-13 06:07:57 +0100

    Media query pt. 2

--- src/components/movieFullDetails/MovieFullDetail.jsx
+++ src/components/movieFullDetails/MovieFullDetail.jsx
@@ -7,22 +7,22 @@ function MovieFullDetail({movieDetails}) {
 
   return (
     <div>
-      <div className='movie_detail_container pb-5 px-5'>
+      <div className='movie_details_container pb-5 px-5'>
         <div>
           <h4>Movie Details</h4>
           <div className="row d-flex justify-content-center border border-light-subtle pt-5 pb-5">
 
-            <div className="col-auto">
+            <div className="col-auto content">
               
-              <div className="d-flex justify-content-center" style={{ width: '250px'}}>
+              <div className="d-flex justify-content-center title_block" style={{ width: '250px'}}>
                 <span>{title}</span>
               </div>
 
               {/* Central block of movie details */}
-              <div className='d-flex flex-row pt-3 pb-3'>
+              <div className='d-flex flex-row central_block pt-3 pb-3'>
                 <img src={poster} alt='movie_poster' style={{ width: '250px', height:"300px"}}/>
                 
-                <div className="details_container d-flex flex-column justify-content-between ms-5">
+                <div className="details_container d-flex flex-column justify-content-between ms-5" style={{textAlign: 'left'}}>
                   <span className="pb-1">Title: {title}</span>
                   <span className="pb-1">Year: {year}</span>
                   <span className="pb-1">Duration: {runtime}</span>
@@ -40,10 +40,9 @@ function MovieFullDetail({movieDetails}) {
                     }
                   </span>
                 </div>
-              
               </div>
               
-              <div className="d-flex justify-content-center" style={{ width: '250px'}}>
+              <div className="d-flex justify-content-center year_block" style={{ width: '250px'}}>
                 <span>{year}</span>
               </div>
                 
